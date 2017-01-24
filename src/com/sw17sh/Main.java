caa404ff9ff8ea4834f0d24b12240818b894644a
stefha <stefan.haeuslmann@gmail.com>
2017-01-24 21:36:59 +0100

    syso changed

--- src/com/sw17sh/Main.java
+++ src/com/sw17sh/Main.java
@@ -3,7 +3,7 @@ package com.sw17sh;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world from Stefan");
         
     }
 }
