e3b57d2d24bcd745e850f59f522ab542dc448fce
dennis <dennis.sommer.86@gmail.com>
2017-01-24 23:52:31 +0100

    WebsiteModel initiated

--- src/main/java/com/sw17sh/Main.java
+++ src/main/java/com/sw17sh/Main.java
@@ -1,23 +1,48 @@
 package com.sw17sh;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sw17sh.model.WebsiteModel;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+
 public class Main {
+    private static String workingJsonLD = "./src/main/java/com/sw17sh/json/InputSearchActionJsonLD";
     private static final Logger LOGGER = Logger.getLogger(Main.class);
-    public static void main(String[] args) {
+
+
+    private static String getJsonLD(String filepath){
         try{
-            String jsonLDSearchAction = new Scanner (new File("./src/main/java/com/sw17sh/json/InputBuyActionJsonLD")).useDelimiter("\\Z").next();
-            System.out.println(jsonLDSearchAction);
+            return new Scanner (new File(filepath)).useDelimiter("\\Z").next();
         }catch (IOException e){
             LOGGER.error("Couldnt load file",e);
         }
+        return null;
+    }
 
-        System.out.println("hello world");
+    private static WebsiteModel getWebsiteModel(String jsonLDWebsite){
+        if(jsonLDWebsite!=null){
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                return objectMapper.readValue(jsonLDWebsite,WebsiteModel.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            LOGGER.error("Empty String cant continue");
+        }
+        return null;
+    }
 
+    public static void main(String[] args) {
+        String jsonLD = getJsonLD(workingJsonLD);
+        WebsiteModel websiteModel = getWebsiteModel(jsonLD);
+        if(websiteModel!=null){
+
+        }
 
 
 
