bebd3185b24fce9e1e19dc8d0b5e77d49389da2b
stefha <stefan.haeuslmann@gmail.com>
2017-01-25 22:41:07 +0100

    changed files to json type , created filledinsearchaction

--- src/main/java/com/sw17sh/model/Event.java
+++ src/main/java/com/sw17sh/model/Event.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Event {
+public class Event extends JsonLDTypeModel {
 
     @JsonProperty("@context")
     private String context;
@@ -15,13 +15,16 @@ public class Event {
 
     @JsonProperty(value = "potentialAction")
     private SearchActionModel searchActionModel;
-    @JsonProperty(value = "startTime")
-    public String startTime;
-    @JsonProperty(value = "endTime")
-    public String endTime;
+
+    @JsonProperty(value = "startDate")
+    public String startDate;
+
+    @JsonProperty(value = "endDate")
+    public String endDate;
 
     @JsonProperty(value = "name")
-    public PropertyValueSpecificationModel queryInput;
+    public String name;
+
     @JsonProperty(value = "location")
     public SearchActionModel.Place location;
 }
--- src/main/java/com/sw17sh/model/WebsiteModel.java
+++ src/main/java/com/sw17sh/model/WebsiteModel.java
@@ -4,9 +4,6 @@ package com.sw17sh.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/**
- * Created by ${Dennis} on 05.12.2016.
- */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WebsiteModel extends JsonLDTypeModel {
 
