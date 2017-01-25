6dfc6460a10385942a338632e3bdf1dd3adc8f91
dennis <dennis.sommer.86@gmail.com>
2017-01-25 15:51:40 +0100

    readJson switchCase

---
+++ src/main/java/com/sw17sh/model/JsonLDType.java
@@ -0,0 +1,23 @@
+package com.sw17sh.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by ${Dennis} on 05.12.2016.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonLDType {
+
+    @JsonProperty("@type")
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
