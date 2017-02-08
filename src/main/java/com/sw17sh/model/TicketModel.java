8e873c2c11debd6452ce85ef2159ac97867371ef
stefha <stefan.haeuslmann@gmail.com>
2017-02-08 20:22:10 +0100

    Added properties to ticket model

--- src/main/java/com/sw17sh/model/TicketModel.java
+++ src/main/java/com/sw17sh/model/TicketModel.java
@@ -11,13 +11,43 @@ public class TicketModel extends JsonLDTypeModel {
     @JsonProperty("underName")
     public String underName;
 
+    @JsonProperty("url")
+    public String url;
+
+    @JsonProperty("name")
+    public String name;
+
+    @JsonProperty("totalPrice")
+    public int totalPrice;
+
+    @JsonProperty("dateIssued")
+    public String dateIssued;
+
+    @JsonProperty("ticketNumber")
+    public int ticketNumber;
+
+
     @JsonProperty(value = "issuedBy")
-    public Object issuedBy;
+    public Organization issuedBy;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Organization extends JsonLDTypeModel {
+
+        @JsonProperty(value = "name")
+        public String name;
+
+        @JsonProperty(value = "url")
+        public String url;
+
+        @JsonProperty(value = "id")
+        public String id;
+
+    }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Object extends JsonLDTypeModel {
 
-        @JsonProperty(value = "@id")
+        @JsonProperty(value = "id")
         public String id;
     }
 
